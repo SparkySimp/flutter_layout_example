c648783e995b21abcdb94f1ebe5d7dc2be82569a
SparkySimp <info@yigitovski.com>
2024-01-12 10:56:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# flutter_layout_example
+
+A demonstration of the Fluytter UI layout elements.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+## The goal I desire to achieve.
+![Application Layout](WhatsApp_Gorsel_2024-01-12_saat_09.06.20_e11141a0.jpg)
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    final media = MediaQuery.of(context);
+
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: Column(
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  color: Colors.blue,
+                  margin: const EdgeInsets.all(20.0),
+                )
+              ],
+            ),
+            Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      padding: const EdgeInsets.all(20),
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                              margin: const EdgeInsets.all(20),
+                              color: Colors.yellow.shade100),
+                          Container(
+                              margin: const EdgeInsets.all(20),
+                              color: Colors.yellow.shade100),
+                          Container(
+                              margin: const EdgeInsets.all(20),
+                              color: Colors.yellow.shade100),
+                        ],
+                      ))
+                ]),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>
+                  [
+                  Container(color: Colors.lightBlue.shade100, height: 
+                ]),
+                Column(),
+                Column(),
+              ],
+            ),
+            Row(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
